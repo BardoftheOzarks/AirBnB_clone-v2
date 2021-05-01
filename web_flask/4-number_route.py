a397f392c8e62a8e266bd1b510aa085fdfe418d7
BardoftheOzarks <1886@holbertonschool.com>
2021-05-01 19:52:51 +0000

    defined int in route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -27,10 +27,9 @@ def cool_it(text='is_cool'):
     return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def numma(n):
-    if type(n) is int:
-        return '{} is a number'.format(n)
+    return '{} is a number'.format(n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
